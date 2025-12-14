5a39901e7a47af8708a571edcd80989a86ea8736
Gaurav Singh <gauravsingh@circuitvalley.com>
2025-12-14 23:14:15 +0100

    Delete Source directory

--- Source/timer2.c
+++
@@ -1,9 +0,0 @@
-
-#include <xc.h>
-#include <stdint.h>
-
-void initTimer2() 
-{
-    T2CONbits.T2CKPS = 0x01; //presacler /4 , Timer is fed with 12M (48M/4) again /4 will have 3M ->333ns/ bit
-    T2CONbits.TMR2ON = 1;
-}
